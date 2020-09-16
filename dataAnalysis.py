18001e3645a623f40ca329573451f5e3b214e09b
bamblebam <burhanizrangwala@gmail.com>
2020-09-16 07:37:31 +0530

    added dataset

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -3,5 +3,10 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+pd.pandas.set_option('display.max_columns', None)
+
+# %%
+dataset = pd.read_csv('dataset/train.csv')
+print(dataset.shape)
 
 # %%
