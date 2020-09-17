6daace1bc17cd1206b0c71359ef55f780190d862
bamblebam <burhanizrangwala@gmail.com>
2020-09-17 22:54:27 +0530

    test changes start

---
+++ testChanges.py
@@ -0,0 +1,8 @@
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.pandas.set_option('display.max_columns', None)
+# %%
+dataset = pd.read_csv('dataset/test.csv')
