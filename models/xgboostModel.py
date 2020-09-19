37c726765c2f5f5de061d01e1f7a661a1c2e019b
bamblebam <burhanizrangwala@gmail.com>
2020-09-19 12:06:33 +0530

    xgboost model done

---
+++ models/xgboostModel.py
@@ -0,0 +1,22 @@
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost
+import sklearn
+import pickle
+pd.pandas.set_option('display.max_columns', None)
+
+# %%
+dataset = pd.read_csv('../dataset/dummytrain.csv')
+# %%
+X_train = dataset.drop(['SalePrice'], axis=1)
+y_train = dataset['SalePrice']
+# %%
+classifier = xgboost.XGBRegressor()
+classifier.fit(X_train, y_train)
+# %%
+filename = 'xgboost_model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+# %%
