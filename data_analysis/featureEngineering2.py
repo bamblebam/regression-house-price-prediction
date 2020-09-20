bf647c6ec571e3d5dfb2032e7739dfdf736896c3
bamblebam <burhanizrangwala@gmail.com>
2020-09-21 01:29:55 +0530

    scaler min max

--- data_analysis/featureEngineering2.py
+++ data_analysis/featureEngineering2.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 pd.pandas.set_option('display.max_columns', None)
 # %%
 dataset = pd.read_csv('../dataset/dummytrain.csv')
@@ -16,4 +17,16 @@ num_features = ['LotFrontage', 'LotArea', '1stFlrSF', 'GrLivArea', 'SalePrice']
 # normalizing log
 for feature in num_features:
     dataset[feature] = np.log(dataset[feature])
+# %%
+scaling_features = [
+    feature for feature in dataset.columns if feature not in ['SalePrice']]
+scaler = MinMaxScaler()
+scaler.fit(dataset[scaling_features])
+# %%
+dataset2 = pd.concat([dataset['SalePrice'].reset_index(drop=True), pd.DataFrame(
+    scaler.transform(dataset[scaling_features]), columns=scaling_features)], axis=1)
+# %%
+dataset2.drop(['Unnamed: 0'], inplace=True, axis=1)
+# %%
+dataset2.head()
 # %%
