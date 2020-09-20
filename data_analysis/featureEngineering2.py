f29b3614ea79a4b5f896179c910e4612a8e0ba4a
bamblebam <burhanizrangwala@gmail.com>
2020-09-20 23:52:44 +0530

    log normal

--- data_analysis/featureEngineering2.py
+++ data_analysis/featureEngineering2.py
@@ -7,3 +7,13 @@ pd.pandas.set_option('display.max_columns', None)
 # %%
 dataset = pd.read_csv('../dataset/dummytrain.csv')
 # %%
+for feature in ['YearBuilt', 'YearRemodAdd', 'GarageYrBlt']:
+    dataset[feature] = dataset['YrSold']-dataset[feature]
+
+# %%
+num_features = ['LotFrontage', 'LotArea', '1stFlrSF', 'GrLivArea', 'SalePrice']
+# %%
+# normalizing log
+for feature in num_features:
+    dataset[feature] = np.log(dataset[feature])
+# %%
