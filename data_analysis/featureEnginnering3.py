f83ac95f097df7c4f064ded42dc8e4397018afc8
bamblebam <burhanizrangwala@gmail.com>
2020-09-24 00:39:29 +0530

    dummy normal without sp

---
+++ data_analysis/featureEnginnering3.py
@@ -0,0 +1,35 @@
+# min max scaling and normalization for dataset from featureEnginnering.py
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+pd.pandas.set_option('display.max_columns', None)
+# %%
+dataset = pd.read_csv('../dataset/dummytrain.csv')
+# %%
+for feature in ['YearBuilt', 'YearRemodAdd', 'GarageYrBlt']:
+    dataset[feature] = dataset['YrSold']-dataset[feature]
+
+# %%
+num_features = ['LotFrontage', 'LotArea', '1stFlrSF', 'GrLivArea']
+# %%
+# normalizing log
+for feature in num_features:
+    dataset[feature] = np.log(dataset[feature])
+# %%
+scaling_features = [
+    feature for feature in dataset.columns if feature not in ['SalePrice']]
+scaler = MinMaxScaler()
+scaler.fit(dataset[scaling_features])
+# %%
+dataset2 = pd.concat([dataset['SalePrice'].reset_index(drop=True), pd.DataFrame(
+    scaler.transform(dataset[scaling_features]), columns=scaling_features)], axis=1)
+# %%
+dataset2.drop(['Unnamed: 0'], inplace=True, axis=1)
+# %%
+dataset2.head()
+# %%
+dataset2.to_csv('../dataset/scaled_train_4.csv', index=False)
+# %%
---
+++ models/xgboostModel3.py
@@ -0,0 +1,34 @@
+# xgboost model for featureEnginnering.py dataset
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost
+import sklearn
+import pickle
+pd.pandas.set_option('display.max_columns', None)
+
+# %%
+dataset = pd.read_csv('../dataset/scaled_train_4.csv')
+# %%
+X_train = dataset.drop(['SalePrice'], axis=1)
+y_train = dataset['SalePrice']
+# %%
+classifier = xgboost.XGBRegressor()
+classifier.fit(X_train, y_train)
+# %%
+filename = '../compiled_models/xgboost_3.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+# %%
+submission_df = dataset = pd.read_csv('../dataset/sample_submission.csv')
+df_test = pd.read_csv('../dataset/scaled_test.csv')
+# %%
+y_pred = classifier.predict(df_test)
+
+# %%
+pred = pd.DataFrame(y_pred)
+sample = pd.concat([submission_df['Id'], pred], axis=1)
+sample.columns = ['Id', 'SalePrice']
+sample.to_csv('../submissions/sample_submission5.csv', index=False)
+# %%
