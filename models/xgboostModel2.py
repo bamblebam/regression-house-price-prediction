733058687700d9e7e4552822535b140f3ead6a30
bamblebam <burhanizrangwala@gmail.com>
2020-09-23 23:53:19 +0530

    scaled model submission

---
+++ models/xgboostModel2.py
@@ -0,0 +1,36 @@
+# xgboost model for featureEnginnering.py dataset
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import xgboost
+import sklearn
+import pickle
+pd.pandas.set_option('display.max_columns', None)
+
+# %%
+dataset = pd.read_csv('../dataset/scaled_train_1.csv')
+# %%
+X_train = dataset.drop(['SalePrice'], axis=1)
+y_train = dataset['SalePrice']
+# %%
+classifier = xgboost.XGBRegressor()
+classifier.fit(X_train, y_train)
+# %%
+filename = '../compiled_models/xgboost_scaled_model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+# %%
+submission_df = dataset = pd.read_csv('../dataset/sample_submission.csv')
+df_test = pd.read_csv('../dataset/scaled_test.csv')
+# %%
+y_pred = classifier.predict(df_test)
+# %%
+y_pred = np.exp(y_pred)
+print(y_pred)
+# %%
+pred = pd.DataFrame(y_pred)
+sample = pd.concat([submission_df['Id'], pred], axis=1)
+sample.columns = ['Id', 'SalePrice']
+sample.to_csv('../submissions/sample_submission2.csv', index=False)
+# %%
