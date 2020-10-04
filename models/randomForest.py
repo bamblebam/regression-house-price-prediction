99b480ecfb00fa0cee9396997b70ca11f679e58e
bamblebam <burhanizrangwala@gmail.com>
2020-10-04 06:43:35 +0530

    diff func

--- models/randomForest.py
+++ models/randomForest.py
@@ -9,8 +9,10 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_log_error
 from hyperopt.pyll.base import scope
 from hyperopt import tpe, hp, fmin, Trials, STATUS_OK
+from sklearn.model_selection import train_test_split
 pd.pandas.set_option('display.max_columns', None)
 
 # %%
@@ -25,15 +27,22 @@ parameter_space = {
     'max_features': hp.randint('max_features', 2, 5),
     'criterion': hp.choice('criterion', ['mse', 'mae']),
 }
+# %%
+submission_df = dataset = pd.read_csv('../dataset/sample_submission.csv')
+df_test = pd.read_csv('../dataset/dummytest.csv')
 
+# %%
+train_X, test_X, train_y, test_y = train_test_split(
+    X_train, y_train, test_size=0.2, random_state=42)
 # %%
 
 
 def HPTuning(params):
     classifier = RandomForestRegressor(**params, n_jobs=1)
-    accuracy = cross_val_score(
-        classifier, X_train, y_train, scoring="neg_mean_absolute_error").mean()
-    return {"loss": -accuracy, 'status': STATUS_OK}
+    classifier.fit(train_X, train_y)
+    y_pred = classifier.predict(test_X)
+    score = np.sqrt(mean_squared_log_error(test_y, y_pred))
+    return {"loss": score, 'status': STATUS_OK}
 
 
 # %%
@@ -47,22 +56,20 @@ best = fmin(
 )
 print("Best: {}".format(best))
 
-# %%
-submission_df = dataset = pd.read_csv('../dataset/sample_submission.csv')
-df_test = pd.read_csv('../dataset/dummytest.csv')
+
 # %%
 regressor = RandomForestRegressor(
-    max_depth=91, max_features=4, n_estimators=200, criterion='mse')
+    max_depth=77, max_features=4, n_estimators=500, criterion='mse')
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(df_test)
 print(y_pred)
 
 # %%
-filename = '../compiled_models/random_forest1.pkl'
+filename = '../compiled_models/random_forest3.pkl'
 pickle.dump(regressor, open(filename, 'wb'))
 # %%
 pred = pd.DataFrame(y_pred)
 sample = pd.concat([submission_df['Id'], pred], axis=1)
 sample.columns = ['Id', 'SalePrice']
-sample.to_csv('../submissions/sample_submission6.csv', index=False)
+sample.to_csv('../submissions/sample_submission8.csv', index=False)
 # %%
